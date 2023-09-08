50f0e06e71cae1b27402de9b093baf63c048e125
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-08 14:47:01 +0200

    The layout for Java CRUD app is designed.

--- src/JavaCrud.java
+++ src/JavaCrud.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class JavaCrud {
+    private JPanel Main;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton saveButton;
+    private JButton deleteButton;
+    private JButton updateButton;
+    private JTextField textField4;
+    private JTextField textField3;
 }
