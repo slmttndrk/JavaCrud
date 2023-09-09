5caa2758f8502ab7d821fa26c0eac67f78b0b595
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-09 10:09:41 +0200

    Mysql database connection is done.

--- src/JavaCrud.java
+++ src/JavaCrud.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class JavaCrud {
     private JPanel Main;
@@ -21,11 +25,28 @@ public class JavaCrud {
     }
 
     public JavaCrud() {
+        Connect();
         saveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
+    }
+
+    Connection con;
+    PreparedStatement pst;
+    public void Connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/gbproducts", "root", "");
+            System.out.println("Success");
+        }
+        catch (ClassNotFoundException ex){
+            ex.printStackTrace();
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
