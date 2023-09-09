ed92c3040953bd3bf4737c25aad73b0eb34d1d2b
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-09 08:47:08 +0200

    The actionListener for Save button is created.

--- src/JavaCrud.java
+++ src/JavaCrud.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class JavaCrud {
     private JPanel Main;
@@ -9,4 +11,21 @@ public class JavaCrud {
     private JButton updateButton;
     private JTextField textField4;
     private JTextField textField3;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JavaCrud");
+        frame.setContentPane(new JavaCrud().Main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public JavaCrud() {
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
