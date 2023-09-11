10ffb26c47f4b82a7c3f3a782ce379bb7d0b3931
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-11 10:11:40 +0200

    Item insertion into the database is done.

--- src/JavaCrud.java
+++ src/JavaCrud.java
@@ -8,13 +8,13 @@ import java.sql.SQLException;
 
 public class JavaCrud {
     private JPanel Main;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField txtName;
+    private JTextField txtPrice;
     private JButton saveButton;
     private JButton deleteButton;
     private JButton updateButton;
     private JTextField textField4;
-    private JTextField textField3;
+    private JTextField txtQty;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("JavaCrud");
@@ -24,18 +24,41 @@ public class JavaCrud {
         frame.setVisible(true);
     }
 
+    Connection con;
+    PreparedStatement pst;
     public JavaCrud() {
         Connect();
         saveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                String name, price, qty;
+
+                name = txtName.getText();
+                price = txtPrice.getText();
+                qty = txtQty.getText();
+
+                try {
+                    pst = con.prepareStatement("insert into products(pname, price, qty) values (?, ? ,?)");
+                    pst.setString(1, name);
+                    pst.setString(2, price);
+                    pst.setString(3, qty);
+                    pst.executeUpdate();
+
+                    JOptionPane.showMessageDialog(null, "Record is added!");
+
+                    txtName.setText("");
+                    txtPrice.setText("");
+                    txtQty.setText("");
+                    txtName.requestFocus();
+                }catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+
             }
         });
     }
 
-    Connection con;
-    PreparedStatement pst;
     public void Connect() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
